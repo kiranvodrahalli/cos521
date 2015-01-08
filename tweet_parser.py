a404947cedfea0bbac0329ef44b3e8b7db3c58ef
Albert Lee <albertlotr@gmail.com>
2015-01-08 12:19:02 -0500

    Fix tweet parser

--- tweet_parser.py
+++ tweet_parser.py
@@ -18,7 +18,7 @@ def parse_tweet_file(tweet_file):
                     tweet_id = unicode(tweet['id'])
                     tweet_timestamp = tweet['created_at']
                     tag_tokens = tweet['text'].strip().split('#')[1:]
-                    tags = [token.split()[0].rstrip('?:!.,;') for token in tag_tokens if token]
+                    tags = [token.split()[0].rstrip('?:!.,;') for token in tag_tokens if token.split()]
                     for tag in tags:
                         out_f.write(','.join([tweet_id, tweet_timestamp, tag+'\n']))
 
