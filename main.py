711b1ad9e040aaaa45d5bd6d117df3c173a1a734
Albert Lee <awlee@princeton.edu>
2015-01-11 13:43:39 -0500

    Add naive novelty detection

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ from naive_trend_predictor import NaiveTrendPredictor
 
 ntp = NaiveTrendPredictor('tweet_data')
 eastern = pytz.timezone('US/Eastern')
-print ntp.get_trending(5, eastern.localize(datetime(2014, 12, 30, 16, 45)),
+print ntp.get_most_popular(5, eastern.localize(datetime(2014, 12, 30, 16, 45)),
+                       eastern.localize(datetime(2014, 12, 30, 17, 0)))
+print ntp.get_most_novel(5, eastern.localize(datetime(2014, 12, 30, 16, 45)),
                        eastern.localize(datetime(2014, 12, 30, 17, 0)))
--- naive_trend_predictor.py
+++ naive_trend_predictor.py
@@ -18,11 +18,40 @@ class NaiveTrendPredictor:
                 self.tweets[hashtag.lower()].append(tweet_dt)
                 self.inverted_tweets_idx[tweet_dt].append(hashtag.lower())
 
-    def get_trending(self, n, start_dt, end_dt):
+
+    def get_hashtags(self, start_dt, end_dt):
         hashtag_grps = (self.inverted_tweets_idx[tweet_dt]
                         for tweet_dt in self.inverted_tweets_idx
                         if start_dt <= tweet_dt and tweet_dt <= end_dt)
-        most_popular = {(sum((start_dt <= tweet_dt and tweet_dt <= end_dt) for tweet_dt in self.tweets[hashtag]), hashtag)
-                        for hashtag_grp in hashtag_grps
-                        for hashtag in hashtag_grp}
+        hashtags = (hashtag for hashtag_grp in hashtag_grps
+                    for hashtag in hashtag_grp)
+        return hashtags
+
+
+    def get_hashtag_freq(self, hashtag, start_dt, end_dt):
+        return sum(start_dt <= tweet_dt and tweet_dt <= end_dt
+                   for tweet_dt in self.tweets[hashtag])
+
+
+    def get_most_popular(self, n, start_dt, end_dt):
+        hashtags = self.get_hashtags(start_dt, end_dt)
+        most_popular = {(self.get_hashtag_freq(hashtag, start_dt, end_dt), hashtag)
+                        for hashtag in hashtags}
         return list(itertools.islice(reversed(sorted(most_popular)), 0, n))
+
+
+    def get_most_novel(self, n, start_dt, end_dt):
+        prev_start_dt = start_dt - (end_dt - start_dt)
+
+        prev_hashtags = self.get_hashtags(prev_start_dt, start_dt)
+        cur_hashtags = self.get_hashtags(start_dt, end_dt)
+
+        prev_freq_counts = {hashtag: self.get_hashtag_freq(hashtag, prev_start_dt, start_dt)
+                            for hashtag in prev_hashtags}
+        cur_freq_counts = {hashtag: self.get_hashtag_freq(hashtag, start_dt, end_dt)
+                           for hashtag in cur_hashtags}
+
+        most_novel = (((cur_freq_counts[hashtag] + 1.0)/(prev_freq_counts.get(hashtag, 0) + len(prev_freq_counts)), hashtag)
+                      for hashtag in cur_freq_counts)
+
+        return list(itertools.islice(reversed(sorted(most_novel)), 0, n))
